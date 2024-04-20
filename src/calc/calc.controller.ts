2cfbac0d7650dc3305123dbc1eb6c54a9ed8bce4
PropsCoder-Raj <panditrohit532@gmail.com>
2024-04-20 12:59:09 +0530

    Update error handling and response structure in CalcController

--- src/calc/calc.controller.ts
+++ src/calc/calc.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, HttpStatus, HttpException } from '@nestjs/common';
 import { CalcService } from './calc.service';
 import { CalcDto } from './calc.dto';
 
@@ -8,9 +8,17 @@ export class CalcController {
 
   @Post('/')
   calc(@Body() calcBody: CalcDto) {
-    const result = this.calcService.calculateExpression(calcBody);
-    return {
-      result,
-    };
+    try {
+      const result = this.calcService.calculateExpression(calcBody);
+      return {
+        result
+      };
+    } catch (error) {
+      throw new HttpException({
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Invalid expression provided',
+        error: 'Bad Request',
+      }, HttpStatus.BAD_REQUEST);
+    }
   }
 }
