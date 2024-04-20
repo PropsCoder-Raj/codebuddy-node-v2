fb8a8f82932a2f6bf9f42a8f758c8493083e67a2
PropsCoder-Raj <panditrohit532@gmail.com>
2024-04-20 12:57:51 +0530

    Update calculation logic in CalcService

--- src/calc/calc.service.ts
+++ src/calc/calc.service.ts
@@ -4,6 +4,34 @@ import { CalcDto } from './calc.dto';
 @Injectable()
 export class CalcService {
   calculateExpression(calcBody: CalcDto) {
-    return 0;
+    if (eval(calcBody.expression)) {
+      const tokens: string[] = calcBody.expression.split(/(\+|\*|\-|\/)/g);
+
+      let result: number = parseInt(tokens[0]);
+
+      for (let i: number = 1; i < tokens.length; i += 2) {
+        const operator: string = tokens[i];
+        const operand: number = parseInt(tokens[i + 1]);
+
+        switch (operator) {
+          case "+":
+            result += operand;
+            break;
+          case "-":
+            result -= operand;
+            break;
+          case "*":
+            result *= operand;
+            break;
+          case "/":
+            result /= operand;
+            break;
+          default:
+            break;
+        }
+      }
+
+      return result ? result : 0;
+    }
   }
 }
